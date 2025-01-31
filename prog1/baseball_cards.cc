34cf20e59ef62d31a880afa1d8dcd578dae786a4
gmaldona <gmaldona@oswego.edu>
2025-01-30 21:00:07 -0500

    Take args + header

--- prog1/baseball_cards.cc
+++ prog1/baseball_cards.cc
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <string>
+#include <getopt.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "baseball_cards.h"
+
+using namespace std;
+
+void compute_max_profit(const string& market_price, const string& price_list) {
+
+}
+
+void compute_max_profit(const string& market_price,
+                        const string& price_list,
+                        const string& output) {
+   return compute_max_profit(market_price, price_list);
+}
+
+
+int main(int argc, char *argv[]) {
+   std::cout << "Hello, World!" << std::endl;
+
+   int opt;
+   string market_price, price_list, output;
+
+   while ((opt = getopt(argc, argv, "m:p:o:")) != -1) {
+      switch (opt) {
+         case 'm':
+            market_price = string(optarg);
+            break;
+         case 'p':
+            price_list = string(optarg);
+            break;
+         case 'o':
+            output = string(optarg);
+            break;
+         default:
+            std::cerr << "[ERROR] bad argument: " << opt << std::endl;
+      }
+   }
+
+   compute_max_profit(market_price, price_list, output);
+
+   return EXIT_SUCCESS;
 }
---
+++ prog1/baseball_cards.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef PROG1__BASEBALL_CARDS_H_
+#define PROG1__BASEBALL_CARDS_H_
+
+#include <string>
+
+void compute_max_profit(const std::string& market_price,
+                        const std::string& price_list,
+                        const std::string& output);
+
+void compute_max_profit(const std::string& market_price,
+                        const std::string& price_list);
+
+#endif //PROG1__BASEBALL_CARDS_H_
