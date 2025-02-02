354b4dff1bdd575394c9a3f1f4361a3d3b88ff52
gmaldona <gmaldona@oswego.edu>
2025-02-02 16:21:58 -0500

    read market_price.txt

--- prog1/baseball_cards.cc
+++ prog1/baseball_cards.cc
@@ -1,27 +1,72 @@
 #include <iostream>
-#include <string>
 #include <getopt.h>
+#include <fstream>
+#include <unistd.h>
+#include <unordered_map>
 
 #include "baseball_cards.h"
 
 using namespace std;
 
-void compute_max_profit(const string& market_price, const string& price_list) {
+ostream& operator<<(ostream& os, market_price_t market_price) {
+   os << "Market Prices: " << std::endl;
+   for (auto& [name, price] : *market_price) {
+      os << name << " : " << price << std::endl;
+   }
+   os << std::endl;
+   return os;
+}
+
+void read_market_price(const string& filename,
+                       const market_price_t& market_price) {
+
+   ifstream market_price_file;
+   string line;
+   size_t line_number = 1;
+
+   market_price_file.open(filename);
+   if (market_price_file.is_open()) {
+
+      while(market_price_file) {
+         getline(market_price_file, line);
+
+         if (line_number > 1) {
+            string name = line.substr(0, line.find(' '));
+            uint64_t price    = stoi(line.substr(line.find(' ') + 1));
+
+            market_price->insert(make_pair(name, price));
+         }
+
+         line_number++;
+      }
+   } else {
+      std::cerr << "Failed to open: " << filename << std::endl;
+   }
+}
+
+void compute_max_profit(const market_price_t& market_price,
+                        const price_list_t&   price_list) {
 
 }
 
-void compute_max_profit(const string& market_price,
-                        const string& price_list,
-                        const string& output) {
+void compute_max_profit(const string& market_price_filename,
+                        const string& price_list_filename,
+                        const string& output_filename) {
+   market_price_t market_price = std::make_shared<__market_price_t>();
+   price_list_t   price_list   = std::make_shared<__price_list_t>();
+
+   read_market_price(market_price_filename, market_price);
+
+   std::cout << market_price << std::endl;
    return compute_max_profit(market_price, price_list);
 }
 
 
 int main(int argc, char *argv[]) {
-   std::cout << "Hello, World!" << std::endl;
-
    int opt;
-   string market_price, price_list, output;
+   std::string market_price;
+   std::string price_list;
+   std::string output;
 
    while ((opt = getopt(argc, argv, "m:p:o:")) != -1) {
       switch (opt) {
--- prog1/baseball_cards.h
+++ prog1/baseball_cards.h
@@ -4,12 +4,71 @@
 #define PROG1__BASEBALL_CARDS_H_
 
 #include <string>
+#include <memory>
+#include <unordered_map>
 
+typedef std::unordered_map<std::string, uint64_t> __market_price_t;
+typedef std::shared_ptr<__market_price_t>           market_price_t;
+
+typedef std::unordered_map<std::string, uint64_t> __price_list_t;
+typedef std::shared_ptr<__price_list_t>             price_list_t;
+
+std::ostream operator<<(std::ostream os, __market_price_t market_price);
+
+std::ostream operator<<(std::ostream os, __price_list_t price_list);
+
+void read_market_price(const std::string& filename,
+                       const market_price_t&  market_price);
+
+/**
+ *
+ * @param market_price is the name of a file that contains baseball card market
+ * prices (that is, the price at which you could sell each card). The first line
+ * of the file contains only an integer that indicates how many cards and prices
+ * reside in the rest of the file, one card per line
+ * @param price_list is the name of a file that contains a sequence of “price
+ * lists” for Gertrude’s baseball cards. You will use the price lists file to
+ * test your program for several potential price lists, so that you are sure you
+ * are ready for whatever price list Gertrude gives you, and for however much
+ * money you will be able to spend.
+ * @param output is the name of a file that contains the output. Each line in
+ * the output file corresponds to a single problem and will contain four numbers
+ * (separated by one space), in the following order:
+        • The size of the input (the number of cards in the price list for this
+            problem)
+        • The computed maximum profit, in integer dollars
+        • The number of cards that you could purchase to achieve the maximum
+            profit
+        • The time (in seconds) it took to solve the problem.
+ */
 void compute_max_profit(const std::string& market_price,
                         const std::string& price_list,
                         const std::string& output);
 
-void compute_max_profit(const std::string& market_price,
-                        const std::string& price_list);
+
+/**
+ * Pseudo-code for maximizing.
+ *
+ * ComputeMaxProfit(I, W):
+ *      maxProfit = 0
+ *      S         = empty set
+ *      M         = empty set
+ *
+ *      if the sum of weights in I <= W:
+ *          return the profit of items in I, and I
+ *      while more subsets to generate:
+ *          if the sum of the weights in S <= W:
+ *              if the profit of the items in S > maxProfit:
+ *                  update maxProfit
+ *
+ *          generate next subset
+ *      return maxProfit and M
+ *
+ * @param market_price
+ * @param price_list
+ */
+
+void compute_max_profit(const market_price_t& market_price,
+                        const price_list_t&   price_list);
 
 #endif //PROG1__BASEBALL_CARDS_H_
