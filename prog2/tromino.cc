80cf04e7beb674d03f4bde344bbb1d734c20d060
gmaldona <gmaldona@oswego.edu>
2025-03-15 21:47:22 -0400

    done

--- prog2/tromino.cc
+++ prog2/tromino.cc
@@ -19,24 +19,187 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <exception>
+
+#include "tromino.h"
 
 // https://stackoverflow.com/questions/600293
 #define IS_POWER_OF_2(x) ((x & (x - 1)) == 0)
 
-#define ORIGIN_X 0
-#define ORIGIN_Y 0
+static board_t board;
+static int     total_board_size;
 
 using namespace std;
 
 //===== GM =========================================================== 80 ====>>
 
+void board_set(const point_t& point, tile_e tile) {
+   board[total_board_size - 1 - point.second][point.first] = tile;
+}
+
+int board_get(const point_t& point) {
+   return board[total_board_size - 1 - point.second][point.first];
+}
+
+tile_e use_tile(const point_t* quad) {
+   if (board_get(quad[0]) != tile_e::EM) {
+      return tile_e::LR;
+   }
+   else if (board_get(quad[1]) != tile_e::EM) {
+      return tile_e::LL;
+   }
+   else if (board_get(quad[2]) != tile_e::EM) {
+      return tile_e::UR;
+   }
+   else if (board_get(quad[3]) != tile_e::EM) {
+      return tile_e::UL;
+   }
+
+   return tile_e::LR;
+}
+
+board_t create_board(const int board_size) {
+   board_t _board = (board_t) malloc(sizeof(board_row_t) * board_size);
+   for  (int row = 0; row < board_size; ++row) {
+      _board[row] = (board_row_t) malloc(sizeof(cell_t) * board_size);
+      for (int col = 0; col < board_size; ++col) {
+         _board[row][col] = tile_e::EM;
+      }
+   }
+   return _board;
+}
+
+void destroy_board(board_t _board, const int board_size) {
+   for  (int row = 0; row < board_size; ++row) {
+      delete _board[row];
+   }
+   delete _board;
+}
+
+inline void display_board(board_t _board, const int board_size) {
+   for  (int row = 0; row < board_size; ++row) {
+      for (int col = 0; col < board_size; ++col) {
+         switch (_board[row][col]) {
+            case MS:
+               std::cout << "MS";
+               break;
+            case EM:
+               std::cout << "XX";
+               break;
+            case LR:
+               std::cout << "LR";
+               break;
+            case LL:
+               std::cout << "LL";
+               break;
+            case UR:
+               std::cout << "UR";
+               break;
+            case UL:
+               std::cout << "UL";
+               break;
+            default:
+               std::cout << "XX"; // null
+               break;
+         }
+         std::cout << " ";
+      }
+      std::cout << std::endl;
+   }
+}
+
 void tromino(const int x_board,   const int y_board,
              const int x_missing, const int y_missing,
              const int board_size) {
 
+   int offset = (board_size / 2);
+   point_t p1 = make_pair(x_board + offset - 1, y_board + offset);
+   point_t p2 = make_pair(x_board + offset,     y_board + offset);
+   point_t p3 = make_pair(x_board + offset - 1, y_board + offset - 1);
+   point_t p4 = make_pair(x_board + offset,     y_board + offset - 1);
+
+   point_t quad[] = {p1, p2, p3, p4};
+   if (board_size == 2) {
+      tile_e tile = use_tile(quad);
+      for (auto& point : quad) {
+         if (board_get(point) == tile_e::EM) {
+            board_set(point, tile);
+         }
+      }
+      return;
+   }
+
+   point_t missing = make_pair(x_missing, y_missing);
+   point_t mid     = make_pair(x_board + offset, y_board + offset);
+
+   tile_e tile;
+
+   if (missing.first <= mid.first && missing.second > mid.second) {
+      tile = tile_e::LR;
+      board_set(p2, tile);
+      board_set(p3, tile);
+      board_set(p4, tile);
+   }
+   else if (missing.first > mid.first && missing.second > mid.second) {
+      tile = tile_e::LL;
+      board_set(p1, tile);
+      board_set(p3, tile);
+      board_set(p4, tile);
+   }
+   else if (missing.first <= mid.first && missing.second <= mid.second) {
+      tile = tile_e::UR;
+      board_set(p1, tile);
+      board_set(p2, tile);
+      board_set(p4, tile);
+   }
+   else if (missing.first > mid.first && missing.second <= mid.second) {
+      tile = tile_e::UL;
+      board_set(p1, tile);
+      board_set(p2, tile);
+      board_set(p3, tile);
+   }
+
+   offset = board_size / 2;
+   // Q1
+   if (board_get(p1) != tile_e::EM && board_get(p1) != tile_e::MS)
+   {
+      tromino(x_board, y_board + offset, p1.first, p1.second, board_size / 2);
+   } else
+   {
+      tromino(x_board, y_board + offset, x_missing, y_missing, board_size / 2);
+   }
+
+   // Q2
+   if (board_get(p2) != tile_e::EM && board_get(p2) != tile_e::MS)
+   {
+      tromino(x_board + offset, y_board + offset, p2.first, p2.second, board_size / 2);
+   } else
+   {
+      tromino(x_board + offset, y_board + offset, x_missing, y_missing, board_size / 2);
+   }
+
+   // Q3
+   if (board_get(p3) != tile_e::EM && board_get(p3) != tile_e::MS)
+   {
+      tromino(x_board, y_board, p3.first, p3.second, board_size / 2);
+   } else
+   {
+      tromino(x_board, y_board, x_missing, y_missing, board_size / 2);
+   }
+
+   // Q4
+   if (board_get(p4) != tile_e::EM && board_get(p4) != tile_e::MS)
+   {
+      tromino(x_board + offset, y_board, p4.first, p4.second, board_size / 2);
+   } else
+   {
+      tromino(x_board + offset, y_board, x_missing, y_missing, board_size / 2);
+   }
+
 }
 
-int main(void) {
+int main() {
 
    /* User input parsing for board size and missing tile (x,y) coordinate */
    int board_size;
@@ -56,11 +219,10 @@ int main(void) {
       return EXIT_FAILURE;
    }
    if (board_size == 0) {
-      std::cerr << "[FINEST] Exiting ...";
       return EXIT_SUCCESS;
    }
 
-   if (! IS_POWER_OF_2(board_size)) {
+   if (! IS_POWER_OF_2(board_size) || board_size < 0) {
       std::cerr << "[ERROR] Board size is not a power of 2. Exiting ...";
       return EXIT_FAILURE;
    }
@@ -85,11 +247,15 @@ int main(void) {
       return EXIT_FAILURE;
    }
 
-   std::cerr <<
-      "[FINEST] Missing tile: (" << x_missing << ", "
-                                 << y_missing << ")" << std::endl;
+   board = create_board(board_size);
+   total_board_size = board_size;
 
-   tromino(ORIGIN_X, ORIGIN_Y, x_missing, y_missing, board_size);
+   board[board_size - y_missing - 1][x_missing] = tile_e::MS;
+
+   tromino(0, 0, x_missing, y_missing, board_size);
+
+   display_board(board, board_size);
+   destroy_board(board, board_size);
 
    return EXIT_SUCCESS;
 }
--- prog2/tromino.h
+++ prog2/tromino.h
@@ -31,7 +31,87 @@
 #ifndef PROG2__TRIMINO_HH_
 #define PROG2__TRIMINO_HH_
 
+#include <stdint.h>
+
 //===== GM =========================================================== 80 ====>>
+
+typedef uint8_t   cell_t;
+typedef uint8_t*  board_row_t;
+/**
+ * NOTE: The board has an origin of (0,0) at the bottom left of the a 2D matrix
+ * e.g. 4x4 matrix will have an origin of (0, 3).
+ *
+ * When setting and getting, an offset MUST be applied to get the correct slot.
+ *
+ * See: #board_get(point_s)
+ * See: #board_set(point_s, tile_e)
+ */
+typedef uint8_t** board_t;
+
+typedef std::pair<int, int> point_t;
+
+enum tile_e {
+   MS  =  0,  // Actual Missing Square
+   LR  =  1,  // Lower   Right
+   LL  =  2,  // Lower   Left
+   UR  =  3,  // Upper   Right
+   UL  =  4,  // Upper   Left
+   EM  =  5,  // EMPTY
+};
+
+/**
+ *
+ * Sets the tile of point on the board. An offset is applied w.r.t board origin.
+ * See: board_t
+ *
+ * @param point point on the board to set
+ * @param tile  set the point to tile
+ */
+void board_set(const point_t& point, tile_e tile);
+
+/**
+ *
+ * Gets the tile of point on the board. An offset is applied w.r.t. board origin
+ * See: board_t
+ *
+ * @param point point on the board to get
+ * @return the point's tile
+ */
+int board_get(const point_t& point);
+
+/**
+ * Returns the correct tile to use given a 2x2 base case and an unavailable tile
+ *
+ * @param quad a list of points in a 2x2 where:
+ *      [0] is top left
+ *      [1] is top right
+ *      [2] is bottom left
+ *      [3] is bottom right
+ * @return A tile to be used
+ */
+tile_e use_tile(const point_t* quad);
+
+/**
+ *
+ * @param board
+ * @param board_size
+ */
+inline void display_board(board_t board, const int board_size);
+
+/**
+ * Allocates memory for tromino board
+ *
+ * @param board_size board size
+ * @return allocated memory for a tromino board of size {@code board_size}
+ */
+board_t create_board(const int board_size);
+
+/**
+ * Frees memory allocated to tromino board
+ *
+ * @param board allocated tromino board
+ */
+void destroy_board(board_t board, const int board_size);
 
 /**
  *
